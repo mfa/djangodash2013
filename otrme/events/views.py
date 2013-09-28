3027324fad657ba583bd89db2025171eb318e228
Ramon Klass <tier@schokokeks.org>
2013-09-28 14:00:41 +0200

    Sse View with comments, please read

--- otrme/events/views.py
+++ otrme/events/views.py
@@ -1,1 +1,25 @@
 # -*- coding: utf-8 -*-
+from django_sse.views import BaseSseView
+from pgnotify import pg_listen
+import json
+
+class PGEventsView(BaseSseView):
+    def dispatch(self, request, *args, **kwargs):
+        # EventSource /events/ should work for all logged in users,
+        # so we should be able to get this from request.user
+        # alternatives welcome
+        self.pg_channel = request.user.username
+
+    def handle_message(self, message):
+        # this can be overridden for alternative behavior
+        # must return a tuple (sse_channel, message)
+        # where message should be json for the frontend
+        # and channel should be a channel that has an attached
+        # EventSource listener on the client side
+        message = json.loads(message)
+        return message[0], message[1]
+
+    def iterator(self):
+        for message in pg_listen(self.pg_channel):
+            message = self.handle_message(message)
+            self.sse.add_message(message[0], message[1])
