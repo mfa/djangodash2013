2d9b52b281e1cb593d623f9d5634447f96ed89f2
Andreas Madsack <andreas@madflex.de>
2013-09-28 18:22:41 +0200

    add some statics (jquery, bootstrap) and a mainview

--- otrme/otrme/urls.py
+++ otrme/otrme/urls.py
@@ -2,13 +2,15 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import MainView
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'otrme.views.home', name='home'),
+    url(r'^$', MainView.as_view(), name='home'),
     # url(r'^otrme/', include('otrme.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
