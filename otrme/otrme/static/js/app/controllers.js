4019a9b1b0d2b3f5d6fbe4a4099016c25a024199
Ramon Klass <tier@schokokeks.org>
2013-09-29 02:51:56 +0200

    input box remembers partial messages per chat now

--- otrme/otrme/static/js/app/controllers.js
+++ otrme/otrme/static/js/app/controllers.js
@@ -14,9 +14,11 @@ angular.module('controllers',[])
 				   time: "Sept 27th, 2013 12:40 AM CET"}]
 			};
       $scope.users = [{name: 'Jarus',
+		       jid: 'otr@jabme.de',
 		       status: "Online",
 		       unread: 0},
 		      {name: 'Gentle',
+		       jid: 'dastier@schokokeks.org',
 		       status: "Away",
 		       unread: 1}
 		     ];
@@ -32,6 +34,16 @@ angular.module('controllers',[])
 	  };
 	  return "";
       };
+
+      $scope.send_message = function(channel, msg) {
+	  var message = {};
+	  var d = new Date();
+	  message['time'] = d.toString();
+	  message['message'] = msg;
+	  message['user'] = 'Gentle';
+	  $scope.messages[channel].push(message);
+      };
+
       $scope.current_user = function() {
 	  for (id in $scope.users) {
 	      if ($scope.users[id].name == $scope.current_user_name) {
@@ -41,8 +53,18 @@ angular.module('controllers',[])
 	  return null;
       };
       $scope.set_current_user = function(user) {
+	  var old_user = $scope.current_user_name;
+	  $scope.cached_input[old_user] = $scope.new_message_text;
+	  if ($scope.cached_input[user]) {
+	      $scope.new_message_text = $scope.cached_input[user];
+	  } else {
+	      $scope.new_message_text = "";
+	  };
 	  $scope.current_user_name = user;
-	  $scope.current_user().unread = "";
+	  $scope.current_user().unread = 0;
       };
+
       $scope.current_user_name = 'Jarus';
+      $scope.new_message_text = '';
+      $scope.cached_input = {};
   }]);
