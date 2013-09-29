ba5f12d1171c773adb5d3cef41d4e6ca2586a44f
Andreas Madsack <andreas@madflex.de>
2013-09-30 00:00:49 +0200

    send NOTIFY on logout

--- otrme/otrme/views.py
+++ otrme/otrme/views.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from django.views.generic import TemplateView
+from events.pgnotify import pg_notify
+from django.contrib.auth.views import logout as authlogout
+
 
 class MainView(TemplateView):
     template_name = "layout.html"
+
+
+def logout(request, *args, **kwargs):
+    event_payload = {
+        'type': 'disconnect',
+        'jid': request.user.username,
+    }
+    pg_notify(
+        'jab-control',
+        event_payload
+    )
+    return authlogout(request, *args, **kwargs)
