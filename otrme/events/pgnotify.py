23cb5bf12abcb1f484ddfdd306838f49cf24c922
Ramon Klass <tier@schokokeks.org>
2013-09-29 04:21:45 +0200

    quote that shit

--- otrme/events/pgnotify.py
+++ otrme/events/pgnotify.py
@@ -8,7 +8,7 @@ def pg_listen(channel):
     crs = connection.cursor()
     pg_con = connection.connection
     pg_con.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-    crs.execute('LISTEN %s;' % channel);
+    crs.execute('LISTEN "%s";' % channel);
 
     while True:
         if select.select([pg_con], [], [], 5) == ([], [], []):
@@ -28,6 +28,6 @@ def pg_notify(channel, payload=None):
     crs = connection.cursor()
     connection.connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
     if payload is None:
-        crs.execute('NOTIFY {};'.format(channel));
+        crs.execute('NOTIFY "{}";'.format(channel));
     else:
-        crs.execute('NOTIFY {}, %s;'.format(channel), [payload]);
+        crs.execute('NOTIFY "{}", %s;'.format(channel), [payload]);
